68144f09b4db82457b9408e37f5b1511454682c7
Christopher Barfield Jr <73442540+barfieldjr@users.noreply.github.com>
2024-06-25 10:39:43 -0400

    Update scripts

--- setup.sh
+++ setup.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-# Create a virtual environment
 python3 -m venv venv
 
-# Activate the virtual environment
 source venv/bin/activate
 
-# Upgrade pip
 pip install --upgrade pip
 
-# Initialize and update the YOLOv5 submodule
 git submodule update --init --recursive
 
-# Install Python dependencies
 pip install -r yolov5/requirements.txt
 pip install -r requirements.txt
--- start.sh
+++ start.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-# Activate the virtual environment
 source venv/bin/activate
 
-# Run the inference script
 python scripts/inference/infer.py "$@"
 
-# Define the video filename and other necessary arguments
 video_filename=$(basename "$1")
 fps="$2"
 
-# Run the clustering script
 python scripts/clustering/converge.py
 
-# Run the clipping script with the video filename
 python scripts/clipping/clip.py "$video_filename"
